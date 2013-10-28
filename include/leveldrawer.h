f1e0bb24d7874f3e8371532b5dd6d822dabbc685
Deadspeller <deadspeller@gmail.com>
2013-10-28 18:23:31 +0100

    prepare for jona

--- include/leveldrawer.h
+++ include/leveldrawer.h
@@ -7,29 +7,30 @@ class LevelDrawer        //class for all classes, Wall-Blocks and Food-Blocks
 
 public:
         
-void drawBlock()
-{
-        /*for(int i = 0; i < levelManager1.blockVector().size(); i++)
-        {
+	void drawBlock()
+	{
+		for(int i = 0; i < levelManager1.blockVector().size(); i++)
+		{
+		
 		}
-                glPushMatrix();
-                glTranslated(blockVec.at(i).xBlockPosition, blockVec.at(i).xyBlockPosition, blockVec.at(i).zBlockPosition);                
-                glScalef(0.5,0.5,0.5);
-                
-                // Block
-                glBegin(GL_POLYGON);
-                if(blocktype == 1)
-                        glColor3f(0,0,1);
-                else
-                        glColor3f(1,1,0);
-                glVertex3f(-1, 1, -1);
-                glVertex3f(1, 1, -1);
-                glVertex3f(1, 1, 1);
-                glVertex3f(-1, 1, 1);
-                glEnd();
-                glPopMatrix();
+		           /*  glPushMatrix();
+		             glTranslated(blockVec.at(i).xBlockPosition, blockVec.at(i).xyBlockPosition, blockVec.at(i).zBlockPosition);                
+		             glScalef(0.5,0.5,0.5);
+		             
+		             // Block
+		             glBegin(GL_POLYGON);
+		             if(blocktype == 1)
+		                     glColor3f(0,0,1);
+		             else
+		                     glColor3f(1,1,0);
+		             glVertex3f(-1, 1, -1);
+		             glVertex3f(1, 1, -1);
+		             glVertex3f(1, 1, 1);
+		             glVertex3f(-1, 1, 1);
+		             glEnd();
+		             glPopMatrix();
 
-                }*/
-		}
+		             }*/
+	}
                 
 }; //end Class
