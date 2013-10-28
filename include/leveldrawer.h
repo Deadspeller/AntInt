e6f000d20fa6bf252246252df35d8fdc5a0d374e
Benedikt <deadspeller@deadspeller-HP-620.(none)>
2013-10-28 09:45:33 +0100

    added leveldrawer.h

---
+++ include/leveldrawer.h
@@ -0,0 +1,34 @@
+
+
+class LevelDrawer	//class for all classes, Wall-Blocks and Food-Blocks
+{
+
+public:
+	
+void drawBlock()
+{
+	for(int i = 0; i < blockVec.size(); i++)
+	{
+		glPushMatrix();
+		glTranslated(xposition, yposition, zposition);		
+		glScalef(0.5,0.5,0.5);
+		
+		// Block
+		glBegin(GL_POLYGON);
+		if(blocktype == 1)
+			glColor3f(0,0,1);
+		else
+			glColor3f(1,1,0);
+		glVertex3f(-1, 1, -1);
+		glVertex3f(1, 1, -1);
+		glVertex3f(1, 1, 1);
+		glVertex3f(-1, 1, 1);
+		glEnd();
+		glPopMatrix();
+
+		}
+
+		
+
+
+}; //end Class
