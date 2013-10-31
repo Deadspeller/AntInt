21b13af1373752eed136d802c46148cdbec6fe96
FreezerJohn <freezerjohn28@gmail.com>
2013-10-31 14:55:37 +0100

    Tidied up Square class in block.h

--- src/block.h
+++ src/block.h
@@ -1,38 +1,23 @@
 #ifndef SQUARE_H
 #define SQUARE_H
 
-#include <GL/glew.h> // Include the GLEW header file  
-//include <SFML/Window.hpp>
-#include <SFML/Graphics.hpp>
-#include <SFML/System.hpp>
 #include <iostream>
-#include <cmath>
-#include <time.h>
-using namespace std;
+#include "ant.h"
 
-extern float xpos, ypos, zpos;
 extern float colobjects[6][4];
-//struct Square
-//	{
-//		int blocktype; //1 = block, 2 = food, 3 = ant, 4 = enemy
-//	};
-
-//extern vector < vector <Square> > worldvector;
 
 class Square	//class for all classes, Wall-Blocks and Food-Blocks
 {
 
 public:
-//	float xBlockPosition, yBlockPosition, zBlockPosition;
-	int blockType;	// 0 = none, 1 = block, 2 = food
+    int blockType;	// 0 = none, 1 = block, 2 = food
+    int food;
+    int smell;
+    Ant* ant;
 
     Square(int typeblock, float xposi, float zposi)
 	{
-		blockType = typeblock;
-			
-//		xBlockPosition = round(xposi);
-//		yBlockPosition = 0.1;
-//		zBlockPosition = round(zposi);
+		blockType = typeblock;		
 	}
 
     Square()
