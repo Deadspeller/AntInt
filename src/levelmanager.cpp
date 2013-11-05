1607ebc97920ac5713b0688803d5030180445cd3
Freezer John <freezerjohn28@gmail.com>
2013-11-05 01:51:34 +0100

    Added wall construction to levelmanager

--- src/levelmanager.cpp
+++ src/levelmanager.cpp
@@ -77,11 +77,26 @@ bool LevelManager::loadFile()
 
             tmpRow.push_back(tmpSquare);
 
-
         }
         worldvector.push_back(tmpRow);
         tmpRow.clear();
     }
+
+    xworldsize = worldvector.size();
+    zworldsize = worldvector.at(0).size();
+
+    //setting up the "walls"
+    for (size_t i = 1; i < zworldsize; i++)
+        worldvector.at(i).at(0).block = 1;
+
+    for (size_t i = 1; i < xworldsize; i++)
+        worldvector.at(0).at(i).block = 1;
+
+    for (size_t i = 1; i < zworldsize; i++)
+        worldvector.at(i).at(xworldsize - 1).block = 1;
+
+    for (size_t i = 1; i < xworldsize; i++)
+        worldvector.at(zworldsize - 1).at(i).block = 1;
 
     mapFile.close();
 }
